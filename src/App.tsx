c16a775448e9f691854c1e185b72905f80893194
thaidzvippro <113168112+hoanGtHaiVip@users.noreply.github.com>
2025-06-09 21:21:51 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from './assets/react.svg'  // nếu react.svg ở src/assets
 import './App.css'
 import SplashCursor from './assets/SplashCursor'
 
@@ -9,12 +8,10 @@ function App() {
 
   return (
     <>
-      {/* Thêm SplashCursor ở đây */}
       <SplashCursor />
-
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
